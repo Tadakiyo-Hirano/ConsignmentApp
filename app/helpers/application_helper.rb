026fa55e8b52b8239d81f626caf7a9259c7bcea8
Tadakiyo-Hirano <d85nus03+sub@gmail.com>
2020-04-08 03:11:36 +0000

    bootstrap追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ブラウザタブタイトル表示
+  def full_title(page_name = "")
+    base_title = "委託商品管理"
+    if page_name.empty?
+      base_title
+    else
+      page_name + " | " + base_title
+    end
+  end
 end
