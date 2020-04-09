69f694c366b9f371a74d4a33720af516fadcb531
Tadakiyo-Hirano <d85nus03+sub@gmail.com>
2020-04-09 07:56:33 +0000

    レスポンシブ設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,29 +9,4 @@ module ApplicationHelper
       page_name + " | " + base_title
     end
   end
-  
-  def sidebar_activate(sidebar_link_url)
-    current_url = request.headers['PATH_INFO']
-    if current_url.match(sidebar_link_url)
-      ' class="active"'
-    else
-      ''
-    end
-  end
-  
-  def sidebar_list_items
-    items = [
-      {text: 'Users', path: root_path},
-      {text: 'Contacts', path: root_path}
-    ]
-
-    html = ''
-    items.each do |item|
-      text = item[:text]
-      path = item[:path]
-      html = html + %Q(<li#{sidebar_activate(path)}><a href="#{path}">#{text}</a></li>)
-    end
-
-    raw(html)
-  end
 end
