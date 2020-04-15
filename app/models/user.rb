3b5593bef02a22a5dfa986a977a797c0d9d73c58
Tadakiyo-Hirano <d85nus03+sub@gmail.com>
2020-04-15 07:50:25 +0000

    views/devise追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :timeoutable
+         
+  validates :name, presence: true, length: { maximum: 50 }
 end
