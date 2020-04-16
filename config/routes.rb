475b5757999d044d346856eb1625cf02cc15c6cc
Tadakiyo-Hirano <d85nus03+sub@gmail.com>
2020-04-16 15:03:11 +0000

    users_controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'top_pages#top'
   
   devise_for :users
-  
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
+  
+  resources :users, only: :show
 
 end
