ecccd00af80b1dac60b30cae5271252a93f0128f
Tadakiyo-Hirano <d85nus03+sub@gmail.com>
2020-04-16 08:36:32 +0000

    新規ユーザー登録,ログアウト機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  # 新規登録時(sign_up時)にnameキーのパラメーターを追加で許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
+  # ログイン後の画面遷移
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+  
+  # ログアウト後の画面遷移
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'top_pages#top'
   
   devise_for :users
+  
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
 end
