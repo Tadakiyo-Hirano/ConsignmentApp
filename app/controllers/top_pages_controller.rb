7397959b71bd3fbddb7c0d0b0250a338bc262afa
Tadakiyo-Hirano <d85nus03+sub@gmail.com>
2020-04-29 07:15:25 +0000

    ハンバーガーメニュー項目レイアウト編集

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,6 +1,6 @@
 class TopPagesController < ApplicationController
   
   def top
-    redirect_to user_url(current_user) if user_signed_in?
+    # redirect_to user_url(current_user) if user_signed_in?
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+   email: 'sample@email.com',
+   name: '管理者',
+   code: 1
+)
+
+num = 1
+5.times do |n|
+  User.create!(
+    email: "sample-#{n + 1}email.com",
+    name: "テスト太郎#{n + 1}",
+    code: num += 1
+  )
+end
+
+puts "Sample user created successfully!"
