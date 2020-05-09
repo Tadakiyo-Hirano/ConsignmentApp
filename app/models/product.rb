f719e0fa7bf81f28f3121bfda33b2bde4e69e6dc
Tadakiyo-Hirano <d85nus03+sub@gmail.com>
2020-05-09 03:01:04 +0000

    product code正規表現作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
-  validates :code, presence: true, length: { maximum: 20 }, uniqueness: true
+  VALID_CODE_REGEX = /\A[A-Z0-9]+\z/
+  validates :code, presence: true, length: { maximum: 20 }, uniqueness: true, format: { with: VALID_CODE_REGEX }
   validates :name, presence: true, length: { maximum: 30 }
   validates :classification, presence: true, length: { maximum: 15 }
   validates :category, presence: true, length: { maximum: 15 }
