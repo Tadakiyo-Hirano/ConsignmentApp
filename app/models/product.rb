095bdb7afb1df515c9c52ee96ea5cd28858aea8c
Tadakiyo-Hirano <d85nus03+sub@gmail.com>
2020-05-12 08:12:08 +0000

    product.code正規表修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   before_save { self.code = code.upcase }
-  VALID_CODE_REGEX = /\A[A-Za-z0-9]+\z/
-  validates :code, presence: true, length: { maximum: 20 }, uniqueness: true, format: { with: VALID_CODE_REGEX }
-  validates :name, presence: true, length: { maximum: 30 }
+  VALID_CODE_REGEX = /\A[\w@-]*[A-Za-z0-9][\w@-]*+\z/
+  validates :code, presence: true, length: { maximum: 13 }, uniqueness: true, format: { with: VALID_CODE_REGEX }
+  validates :name, presence: true, length: { maximum: 40 }
   validates :classification, presence: true, length: { maximum: 15 }
   validates :category, presence: true, length: { maximum: 15 }
 end
