32677bf1e47d0aaf9eb7a10f7c7868ec0b7e0247
Tadakiyo-Hirano <d85nus03+sub@gmail.com>
2020-05-20 14:36:21 +0000

    委託情報登録ページフォーム作成中

--- app/controllers/consignments_controller.rb
+++ app/controllers/consignments_controller.rb
@@ -1,9 +1,27 @@
 class ConsignmentsController < ApplicationController
+  before_action :set_user
   
   def index
-    @user = User.find(params[:id])
   end
   
   def new
+    @consignment = Consignment.new
   end
+  
+  def create
+    @user = Consignment.new(consignment_params)
+    # if @consignment.save
+      flash[:notice] = "登録完了。"
+      redirect_to user_url
+    # else
+    #   flash.now[:alert] = "更新に失敗しました。"
+    #   render :new
+    # end
+  end
+  
+  private
+  
+    def consignment_params
+      params.require(:user).permit(consignments: [:ship_date, :customer_info, :product_info, :serial_number, :note, :quantity])[:consignments]
+    end
 end
--- app/models/consignment.rb
+++ app/models/consignment.rb
@@ -1,3 +1,10 @@
 class Consignment < ApplicationRecord
   belongs_to :user
+  
+  validates :ship_date, presence: true
+  validates :customer_info, presence: true
+  validates :product_info, presence: true
+  validates :serial_number, length: { maximum: 50 }
+  validates :note, length: { maximum: 100 }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
