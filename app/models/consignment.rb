e3b99f59337282eb1c1d72cf3186df0a34648210
Tadakiyo-Hirano <d85nus03+sub@gmail.com>
2020-06-07 08:32:21 +0000

    新規登録画面モーダル検索機能大枠完成

--- app/models/consignment.rb
+++ app/models/consignment.rb
@@ -3,9 +3,11 @@ class Consignment < ApplicationRecord
   
   validates :ship_date, presence: true
   validates :customer_id_number, presence: true
-  validates :customer_info, presence: true
-  validates :customer_name, presence: true
-  validates :product_info, presence: true
+  validates :customer_code, presence: true, length: {maximum: 13 }
+  validates :customer_name, presence: true, length: {maximum: 40 }
+  validates :product_id_number, presence: true
+  validates :product_code, presence: true, length: {maximum: 13 }
+  validates :product_name, presence: true, length: {maximum: 40 }
   validates :serial_number, length: { maximum: 50 }
   validates :note, length: { maximum: 100 }
   validates :quantity, presence: true, numericality: { greater_than: 0 }
