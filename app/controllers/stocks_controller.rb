7bbb61873e56f9f0242b419fbf16113eb664b37b
Tadakiyo-Hirano <d85nus03@gmail.com>
2020-06-23 14:39:55 +0000

    stock view params id紐付け作成

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,26 @@
 class StocksController < ApplicationController
+  before_action :signed_in_user
+  before_action :set_user_stocks
+  # before_action :set_consignment_stocks
+  before_action :signed_in_correct_user
+  
+  def index
+    @stocks = @user.consignments.find(params[:consignment_id]).stocks
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  private
+    
+    def set_user_stocks
+      @user = User.find(params[:user_id])
+    end
+    
+    # def set_consignment_stocks
+    #   @consignment = Consignment(params[:consignment_id])
+    # end
 end
--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,2 +1,7 @@
 module StocksHelper
+  
+  # 委託残数
+  def stock_result(object)
+    object.consignment_quantity - object.sales_quantity - object.return_quantity
+  end
 end
