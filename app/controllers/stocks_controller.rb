8dea323e3cd68bf276d2e71aacb50292ac733b82
Tadakiyo-Hirano <d85nus03@gmail.com>
2020-06-28 08:30:25 +0000

    stock newメソッド作成中

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,15 +9,29 @@ class StocksController < ApplicationController
   end
   
   def new
+    @stock = Stock.new
   end
   
   def create
+    @stock = Stock.new(stock_params)
+    # @stock = Srotck.new(stock_params)
+    if @stock.save
+      flash[:notice] = "登録完了。"
+      redirect_to @user
+    else
+      flash.now[:alert] = "更新に失敗しました。<br> " + @stock.errors.full_messages.join("<br>")
+      render :new
+    end
   end
   
   private
     
     def set_user_stocks
       @user = User.find(params[:user_id])
+    end
+    
+    def stock_params
+      params.require(:stock).permit(:processing_date, :return_quantity, :sales_quantity, :consignment_id)
     end
     
     # def set_consignment_stocks
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,7 @@
 class Stock < ApplicationRecord
   belongs_to :consignment
   
+  validates :processing_date, presence: true
   validates :consignment_quantity, presence: true, numericality: { greater_than: 0 }
   validates :return_quantity, presence: true, numericality: { greater_than: -1 }
   validates :sales_quantity, presence: true, numericality: { greater_than: -1 }
