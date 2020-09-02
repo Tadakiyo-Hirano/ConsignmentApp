5fb283ca80dcf8a0b013f7b09648502ee44f414c
Tadakiyo-Hirano <d85nus03@gmail.com>
2020-09-02 15:03:05 +0000

    deviseログイン周り修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  require 'line/bot'
-  
   include ApplicationHelper
   include UsersHelper
   include CustomersHelper
@@ -9,43 +7,6 @@ class ApplicationController < ActionController::Base
   include StocksHelper
   
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  # callbackアクションのCSRFトークン認証を無効
-  protect_from_forgery :except => [:callback]
-
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
-
-  def callback
-    body = request.body.read
-
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      head :bad_request
-    end
-
-    events = client.parse_events_from(body)
-
-    events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
-        end
-      end
-    }
-
-    head :ok
-  end
   
   # PCA-Web API
   def pca_api
--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -4,8 +4,8 @@ class LinebotController < ApplicationController
   
   def client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ""
-      config.channel_token = ""
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
   
