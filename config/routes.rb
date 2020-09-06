33581c963a16b0899619b7f1f96568e28d5ed973
Tadakiyo-Hirano <d85nus03@gmail.com>
2020-09-06 07:55:43 +0000

    得意先別、商品別レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
-    # registrations: 'users/registrations' # ユーザー情報更新
+    registrations: 'users/registrations' # ユーザー情報更新
   }
   
   devise_scope :user do
