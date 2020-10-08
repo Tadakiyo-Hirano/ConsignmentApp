41c501ec015d77f07e665b0da977df8e1530ce51
Tadakiyo-Hirano <d85nus03@gmail.com>
2020-10-08 14:21:29 +0000

    reminder用メソッド作成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  
+  def reminder_consignments
+    month = Post.find(1).reminder_month.month
+    @user_consignments.each do |c|
+     @aaa = Consignment.where("ship_date < ?", Date.current - month)
+    end
+    @aaa
+  end
 end
