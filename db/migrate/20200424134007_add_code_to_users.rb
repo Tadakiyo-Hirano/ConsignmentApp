e822665f10a8872cd2b381bca7177ba8b7183f32
Tadakiyo-Hirano <d85nus03@gmail.com>
2020-10-16 14:50:30 +0000

    migration users integerスペルミス修正

--- db/migrate/20200424134007_add_code_to_users.rb
+++ db/migrate/20200424134007_add_code_to_users.rb
@@ -1,5 +1,5 @@
 class AddCodeToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :code, :integr, default: 0, null: false, unique: true
+    add_column :users, :code, :integer, default: 0, null: false, unique: true
   end
 end
