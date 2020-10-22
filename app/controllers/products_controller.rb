0e149fdfe7dc682a7ab7e522c13ab847e3a85b80
Tadakiyo-Hirano <d85nus03@gmail.com>
2020-10-22 14:50:56 +0000

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,7 +48,11 @@ class ProductsController < ApplicationController
       # 商品モデル(Product)を更新した場合、更新したProduct.idとProduct.product_id_numberと同じ委託(モデルconsignment)の商品コード、商品名も同時更新する。
       # Consignment.where(['product_id_number == ?', @product.id]).update_all(product_code: Product.find(@product.id).code, product_name: Product.find(@product.id).name)
       # Consignment.where(['cast(product_id_number as text) == ?', @product.id]).update_all(product_code: Product.find(@product.id).code ,product_name: Product.find(@product.id).name) if Consignment.where(product_id_number: @product.id).present?
-      Consignment.where(['CAST(product_id_number AS text) == ?', @product.id.to_s]).update_all(product_code: Product.find(@product.id).code, product_name: Product.find(@product.id).name) if Consignment.where(['CAST(product_id_number AS text) == ?', @product.id.to_s])
+      if Rails.env.production?
+        Consignment.where(['CAST(product_id_number AS text) == ?', @product.id::text]).update_all(product_code: Product.find(@product.id).code, product_name: Product.find(@product.id).name) if Consignment.where(['CAST(product_id_number AS text) == ?', @product.id::text])
+      else Rails.env.development? || Rails.env.test?
+        Consignment.where(['product_id_number == ?', @product.id]).update_all(product_code: Product.find(@product.id).code, product_name: Product.find(@product.id).name) if Consignment.where(['product_id_number == ?', @product.id])
+      end
       flash[:notice] = "【#{@product.code} / #{@product.name}】の情報を更新しました。"
       redirect_to products_url
     elsif @product.name.blank?
