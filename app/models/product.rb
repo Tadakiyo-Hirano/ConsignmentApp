98b1cd5504d310ccd085fcc3570fb713dd909712
Tadakiyo-Hirano <d85nus03@gmail.com>
2020-10-22 07:10:20 +0000

    csv

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,20 +13,47 @@ class Product < ApplicationRecord
   scope :code_like, -> (code) { where('code LIKE ?', "%#{code}%") if code.present? }
   
   # CSVインポート
+  # def self.import(file)
+  #   imported_num = 0
+  #   CSV.foreach(file.path, headers: true, encoding: 'r:CP932') do |row|
+  #     # IDが見つかれば、レコード呼出し、見つかれなければ、新しく作成
+  #     product = find_by(code: row["code"]) || new 
+  #     # CSVからデータを取得し、設定する
+  #     product.attributes = row.to_hash.slice(*updatable_attributes)
+  #     product.save
+  #     imported_num += 1
+  #   end
+  #   imported_num
+  # end
+  
+  # def self.updatable_attributes
+  #   ["code", "name"]
+  # end
   def self.import(file)
     imported_num = 0
-    CSV.foreach(file.path, headers: true, encoding: 'r:CP932') do |row|
-      # IDが見つかれば、レコード呼出し、見つかれなければ、新しく作成
-      product = find_by(code: row["code"]) || new 
-      # CSVからデータを取得し、設定する
-      product.attributes = row.to_hash.slice(*updatable_attributes)
-      product.save
-      imported_num += 1
+
+    open(file.path, 'r:cp932:utf-8', undef: :replace) do |f|
+      csv = CSV.new(f, :headers => :first_row)
+      begin
+        csv.each do |row|
+          next if row.header_row?
+          table = Hash[[row.headers, row.fields].transpose]
+
+          product = find_by(code: table["code"])
+          if product.nil?
+            product = new
+          end
+
+          product.attributes = table.to_hash.slice(*table.to_hash.except(:code).keys)
+
+          if product.valid?
+            product.save!
+            imported_num += 1
+          end
+        end
+      rescue
+      end
     end
     imported_num
-  end
-  
-  def self.updatable_attributes
-    ["code", "name"]
   end
 end
